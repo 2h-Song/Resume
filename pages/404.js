e0c8d7c190e2088524b8304048b25dd3bbdf0e74
송해현 <jea752@daum.net>
2022-12-04 22:40:11 +0900

    'v1.0'

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function NotFoundPage(){
+    return(
+        <div>
+            <h1>페이지를 찾을  수 없습니다 ㅠㅠ</h1>
+        </div>
+    );
+}
---
+++ pages/about-me.js
@@ -0,0 +1,7 @@
+export default function AboutMe(){ // AboutMe가 바깥에서 쓰일수 있도록
+    return(
+        <div>
+            <h1>자기소개</h1>
+        </div>
+    );
+}
