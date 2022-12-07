715bb313649559b1dcfb24514c738a60f7917992
송해현 <jea752@daum.net>
2022-12-08 03:14:47 +0900

    '배포전'

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,9 @@
 import Layout from "../pages/components/layout";
 import Head from "next/head";
-export default function Projects() {
+import { TOKEN, DATABASE_ID } from "../config/index"
+
+export default function Projects({projects}) {
+
     return(
         <Layout>
         <Head>
@@ -8,6 +11,57 @@ export default function Projects() {
             <meta name="description" content="송해현 포트폴리오" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+                    총 프로젝트 :
+                    <span className="pl-4 text-blue-500">{projects.results.length}</span>
+                </h1>
+                {projects.results.map((aProject)=>
+                    <h1>{aProject.properties.Name.title[0]?.plain_text}</h1>
+                )}
         </Layout>
     );
 }
+// 각 요청 때마다 호출
+export async function getServerSideProps({}) {
+
+    
+
+    const options = {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'Notion-Version': '2022-02-22',
+          'content-type': 'application/json',
+          Authorization : `Bearer ${TOKEN}`
+        },
+        body: JSON.stringify({
+            sorts: [
+                {
+                    "property" : "Name",
+                    "direction": "ascending"
+                }
+            ],
+            page_size: 100
+        })
+      };
+      
+      const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+      
+      const projects = await res.json()
+     
+      const projectNames = projects.results.map(
+        (aProject) => aProject.properties.Name.title[0]?.plain_text
+        // ?조건문 
+      );
+
+      console.log(`projectNames : ${projectNames}`);
+
+    return {
+      props: {projects}, // will be passed to the page component as props
+      // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
+      // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
+    }
+}
+
+
+
